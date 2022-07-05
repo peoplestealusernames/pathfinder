d29b8b683d34572a7a8514d7a56adc39aed9dc3e
Brett <bretthoganpr@gmail.com>
2022-07-05 07:34:34 -0500

    Missing TODO

--- src/ToggleGrid.tsx
+++ src/ToggleGrid.tsx
@@ -1,7 +1,7 @@
 import { ToggleBlock } from "./ToggleBlock"
 
 export function ToggleGrid(props: { x: number, y: number }) {
-
+    //TODO: make styles (including height from here pass to toggle block)
     return (
         <div style={{ margin: "0px", padding: "0px", height: 20 }}>
             {
