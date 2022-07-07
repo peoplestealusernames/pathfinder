08dd49b48328d140ae228ec3694ef4b6195a0e42
Brett <bretthoganpr@gmail.com>
2022-07-06 19:29:47 -0500

    Swaped to canvas (no click yet)

--- src/ToggleGrid.tsx
+++ src/ToggleGrid.tsx
@@ -1,33 +1,70 @@
-import { ToggleBlock } from "./ToggleBlock"
-import { Tile } from "./types"
+import { useEffect } from "react"
+import { SwapTable, Tile } from "./types"
 
 export function ToggleGrid(props: { grid: Tile[][], update: any }) {
     //TODO: make styles (including height from here pass to toggle block)
 
+    useEffect(() => {
+        UpdateCanvas(props.grid)
+    }, [props.grid])
+
+    useEffect(() => {
+        const elem = document.getElementById('GridCanvas') as any
+        if (!elem)
+            return
+
+        const elemLeft = elem.offsetLeft + elem.clientLeft
+        const elemTop = elem.offsetTop + elem.clientTop
+        const context = elem.getContext('2d') as CanvasRenderingContext2D
+
+        UpdateCanvas(props.grid)
+
+        elem.addEventListener("click", (e: any) => {
+            const x = e.pageX - elemLeft
+            const y = e.pageY - elemTop
+
+            console.log("CLICKED");
+        }, false)
+    })
+
     return (
-        <div key={`Grid:${props.grid.length}x${props.grid[0].length}`} style={{
+        <div key={`A`} style={{
             margin: "0px", padding: "0px",
-            display: "flex",
-            flexWrap: "nowrap",
-            flexDirection: "column",
+            display: "flex", flexWrap: "nowrap", flexDirection: "row",
+            justifyContent: "center"
         }}>
-            {
-                props.grid.map((row, i) => {
-                    let y = i
-                    return (<div key={`ButtonRow:${y}`} style={{
-                        margin: "0px", padding: "0px", height: 20,
-                        display: "flex", flexWrap: "nowrap", flexDirection: "row",
-                        justifyContent: "center"
-                    }}>
-                        {
-                            row.map((tile, x) => {
-                                return (<ToggleBlock key={`TButton:${x},${y}`} tile={tile} update={props.update} />)
-                            })
-                        }
-                    </div>
-                    )
-                })
-            }
-        </div >
+            <canvas style={{ display: "flow" }} id="GridCanvas" />
+        </div>
     )
+}
+
+function UpdateCanvas(grid: Tile[][]) {
+    const elem = document.getElementById('GridCanvas') as any
+    if (!elem)
+        throw new Error("Cannot find canvas")
+
+    //TODO: scale to size
+    //TODO:math to make them aways square and always right size
+    const width = grid.length * 10
+    const height = grid[0].length * 10
+
+    elem.width = width
+    elem.height = height
+
+    const context = elem.getContext('2d') as CanvasRenderingContext2D
+
+    const img = new Image()
+    context.drawImage(img, 0, 0, img.width, img.height)
+
+    for (const xs in grid) {
+        const x = parseInt(xs)
+        for (const ys in grid[x]) {
+            const y = parseInt(ys)
+            const tile = grid[x][y]
+
+
+            context.fillStyle = SwapTable[tile.state]
+            context.fillRect(x * 10, y * 10, 10, 10)
+        }
+    }
 }
