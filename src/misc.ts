75fc19255804b0cbd730495471c8af99eec5f0f7
Brett <bretthoganpr@gmail.com>
2022-07-10 02:28:54 -0500

    sleep method

--- src/misc.ts
+++ src/misc.ts
@@ -2,3 +2,9 @@
 export function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
 }
+
+export function sleep(ms: number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
