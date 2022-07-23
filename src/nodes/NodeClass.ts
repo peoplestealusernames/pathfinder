bbf8dce0320290f16108f79197d7c4b329b21ced
Brett <bretthoganpr@gmail.com>
2022-07-23 04:35:03 -0500

    Removed bug causing and unneeded ?

--- src/nodes/NodeClass.ts
+++ src/nodes/NodeClass.ts
@@ -3,13 +3,13 @@ import { ParrentChildClass } from "./PCClass"
 
 export class Node<Data extends any> extends ParrentChildClass<Node<Data>> {
     weight: number
-    data?: Data
+    data: Data
     readonly id: string
 
     //child nodes are all nodes that it can move to
     //parent nodes are all nodes that refrence it as a valid child
     //parents are generated automaticlly 
-    constructor(uniqueID: string, weight: number, data?: Data, children: Node<Data>[] = []) {
+    constructor(uniqueID: string, weight: number, data: Data, children: Node<Data>[] = []) {
         super()
         this.id = uniqueID
         this.weight = weight
