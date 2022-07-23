ebeb7133b2cca8d70c90e271d1a02eb4c01bee9c
Brett <bretthoganpr@gmail.com>
2022-07-23 04:25:11 -0500

    TODOs

--- src/backend/events.ts
+++ src/backend/events.ts
@@ -1,12 +1,14 @@
 import EventEmitter from "events";
 
 export class TypedEventEmitter<Events extends Record<string, (...args: any[]) => void>> extends EventEmitter {
+    //TODO: all event option
     on<K extends Exclude<keyof Events, number>>(eventName: K, listener: Events[K]): this {
         return super.on(eventName, listener)
     }
     off<K extends Exclude<keyof Events, number>>(eventName: K, listener: Events[K]): this {
         return super.on(eventName, listener)
     }
+    //TODO: fix emit not requiring some args
     emit<K extends Exclude<keyof Events, number>>(eventName: K, ...args: Parameters<Events[K]>): boolean {
         return super.emit(eventName, ...args)
     }
