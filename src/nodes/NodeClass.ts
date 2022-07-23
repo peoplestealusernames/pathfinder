1e7c8a6e1ae4336f067fad3e18b998ed7b086558
Brett <bretthoganpr@gmail.com>
2022-07-22 21:56:19 -0500

    Moved around contructor to fix future issues

--- src/nodes/NodeClass.ts
+++ src/nodes/NodeClass.ts
@@ -12,8 +12,8 @@ export class Node<Data extends any> extends ParrentChildClass<Node<Data>> {
     constructor(uniqueID: string, weight: number, data?: Data, children: Node<Data>[] = []) {
         super()
         this.id = uniqueID
-        this.addChilds(...children)
         this.weight = weight
         this.data = data
+        this.addChilds(...children)
     }
 }
