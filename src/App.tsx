34870e12904e25999ec9e226ead543bb3bf51c93
Brett <bretthoganpr@gmail.com>
2022-07-24 22:25:53 -0500

    Ported setup to its own FNC

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React, { useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import './App.css';
 import { Buttons } from './Buttons/Buttons';
-import { LayerManger } from './2d/LayerManger';
 import { ToggleGrid } from './ToggleGrid';
-import { CanvasManager } from './2d/canvasManger';
-import { FloodFill } from './pathfinders/floodfill';
-import { GridToNode2d } from './nodes/gridToNode';
 import { Popup } from './components/Popup';
 import { BiCodeBlock } from 'react-icons/bi'
 import { TbReplace } from 'react-icons/tb'
 import { GrStatusPlaceholderSmall } from 'react-icons/gr'
 import { SelectTile } from './Buttons/SelectTile';
 import { Selectable } from './backend/types';
-
-const Movement: [number, number][] = [
-  [1, 0],
-  [0, 1],
-  [-1, 0],
-  [0, -1],
-]
+import { setup } from './setup';
 
 function App() {
   const [InfoPopupState, setInfoPopupState] = useState<boolean>(false)
@@ -27,68 +17,7 @@ function App() {
 
   const [SelectorState, setSelectorState] = useState<Selectable>("empty")
 
-  const [Grid, CanvasMang, Nav] = useMemo(() => {
-    const Grid = new LayerManger(75, 25)
-    const CanvasMang = new CanvasManager(Grid)
-    let Nodes = GridToNode2d(Grid.BaseGrid, Movement)
-    const Start = Nodes.get(...Grid.getStart())
-    const Finish = Nodes.get(...Grid.getGoal())
-
-    //TODO: remove patch work
-    if (!(Start && Finish)) {
-      throw new Error("no start or finish")
-    }
-
-    const Nav = new FloodFill(
-      Start,
-      Finish
-    )
-
-    Grid.on("sizeChange", () => {
-      Nodes = GridToNode2d(Grid.BaseGrid, Movement)
-      const Start = Nodes.get(...Grid.getStart())
-      const Finish = Nodes.get(...Grid.getGoal())
-
-      if (!Start || !Finish)
-        return
-
-      Nav.setStart(Start)
-      Nav.setGoal(Finish)
-    })
-
-    Grid.on("startMove", (x, y) => {
-      const tile = Nodes.get(x, y)
-      if (tile)
-        Nav.setStart(tile)
-    })
-
-    Grid.on("goalMove", (x, y) => {
-      const tile = Nodes.get(x, y)
-      if (tile)
-        Nav.setGoal(tile)
-    })
-
-    Grid.BaseGrid.on("clear", () => {
-      Nav.reset()
-    })
-    Grid.on("clear", () => {
-      Nav.reset()
-    })
-
-    Nav.on("update", (node, state) => { Grid.NavGrid.set(node.data.x, node.data.y, state) })
-    Nav.on("reset", () => { Grid.NavGrid.clear() })
-    Nav.on("solved", (solvedStack) => {
-      while (true) {
-        const node = solvedStack.pop()
-        if (!node)
-          return
-
-        Grid.NavGrid.set(node.data.x, node.data.y, "solved")
-      }
-    })
-
-    return [Grid, CanvasMang, Nav]
-  }, [])
+  const [Grid, CanvasMang, Nav] = useMemo(() => { return setup() }, [])
 
   return (
     <div className="App" style={{ display: "flex", flexDirection: "column", flexWrap: "wrap" }}>
---
+++ src/setup.ts
@@ -0,0 +1,77 @@
+import { useMemo } from "react"
+import { CanvasManager } from "./2d/canvasManger"
+import { LayerManger } from "./2d/LayerManger"
+import { xy } from "./backend/types"
+import { GridToNode2d } from "./nodes/gridToNode"
+import { FloodFill } from "./pathfinders/floodfill"
+import { NavInterface } from "./pathfinders/NavInterface"
+
+const Movement: [number, number][] = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+]
+
+export function setup(): [LayerManger, CanvasManager, NavInterface<xy>] {
+    const Grid = new LayerManger(75, 25)
+    const CanvasMang = new CanvasManager(Grid)
+    let Nodes = GridToNode2d(Grid.BaseGrid, Movement)
+    const Start = Nodes.get(...Grid.getStart())
+    const Finish = Nodes.get(...Grid.getGoal())
+
+    //TODO: remove patch work
+    if (!(Start && Finish)) {
+        throw new Error("no start or finish")
+    }
+
+    const Nav = new FloodFill(
+        Start,
+        Finish
+    )
+
+    Grid.on("sizeChange", () => {
+        Nodes = GridToNode2d(Grid.BaseGrid, Movement)
+        const Start = Nodes.get(...Grid.getStart())
+        const Finish = Nodes.get(...Grid.getGoal())
+
+        if (!Start || !Finish)
+            return
+
+        Nav.setStart(Start)
+        Nav.setGoal(Finish)
+    })
+
+    Grid.on("startMove", (x, y) => {
+        const tile = Nodes.get(x, y)
+        if (tile)
+            Nav.setStart(tile)
+    })
+
+    Grid.on("goalMove", (x, y) => {
+        const tile = Nodes.get(x, y)
+        if (tile)
+            Nav.setGoal(tile)
+    })
+
+    Grid.BaseGrid.on("clear", () => {
+        Nav.reset()
+    })
+    Grid.on("clear", () => {
+        Nav.reset()
+    })
+
+    Nav.on("update", (node, state) => { Grid.NavGrid.set(node.data.x, node.data.y, state) })
+    Nav.on("reset", () => { Grid.NavGrid.clear() })
+    Nav.on("solved", (solvedStack) => {
+        while (true) {
+            const node = solvedStack.pop()
+            if (!node)
+                return
+
+            Grid.NavGrid.set(node.data.x, node.data.y, "solved")
+        }
+    })
+
+    return [Grid, CanvasMang, Nav]
+}
