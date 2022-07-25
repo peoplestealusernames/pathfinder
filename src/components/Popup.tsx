03ac5d7ee5a68779f729fb61ed2bd3d95904683d
Brett <bretthoganpr@gmail.com>
2022-07-24 19:59:29 -0500

    Escape closes popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -4,6 +4,23 @@ import { CenterDiv } from "./CenterDiv";
 
 
 export function Popup(props: { active: boolean, setActive: Dispatch<SetStateAction<boolean>>, children?: React.ReactNode }) {
+    const KeyPressed = (e: KeyboardEvent) => {
+        if (e.key === "Escape") {
+            props.setActive(false)
+        }
+    }
+
+    useEffect(() => {
+        if (props.active)
+            window.addEventListener('keydown', KeyPressed)
+        else
+            window.removeEventListener('keydown', KeyPressed)
+
+        return () => {
+            window.removeEventListener('keydown', KeyPressed)
+        }
+    }, [props.active])
+
     return (<div className="Popup">
         {
             props.active &&
