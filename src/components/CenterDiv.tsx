59fc7f1666b7e282e087cdccc4f14e36da000f6e
Brett <bretthoganpr@gmail.com>
2022-07-25 00:59:35 -0500

    Added styleOverride prop

--- src/components/CenterDiv.tsx
+++ src/components/CenterDiv.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 
 
-export function CenterDiv(props: { children?: React.ReactNode }) {
+export function CenterDiv(props: {
+    children?: React.ReactNode
+    styleOverride?: React.CSSProperties
+}) {
 
     return (
         <div style={{
-            display: "flex",
-            alignItems: "center",
-            justifyItems: "center",
-            justifyContent: "center",
-            height: "100vh",
-            width: "100vw"
+            ...{
+                display: "flex",
+                alignItems: "center",
+                justifyItems: "center",
+                justifyContent: "center",
+                height: "100%",
+                width: "100%"
+            },
+            ...props.styleOverride
         }}>
             {props.children}
-        </div>
+        </div >
     )
 }
