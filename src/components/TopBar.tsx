6aaf587572083b1b1463fbe05f0b455b8bcdc8ab
Brett <bretthoganpr@gmail.com>
2022-07-25 00:35:50 -0500

    Changed TopBar to static

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,7 +6,7 @@ export function TopBar(props: {
 }) {
 
     return (<div className='TopBar' style={{
-        display: "flex", position: "fixed", width: "100vw", height: "50px",
+        display: "flex", position: "static", width: "100vw", height: "50px",
         alignItems: "center",
         justifyItems: "center",
         justifyContent: "center",
