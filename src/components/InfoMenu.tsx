f22e12c3f36f1317786e6c322bf86d89a8088f0c
Brett <bretthoganpr@gmail.com>
2022-07-25 02:16:29 -0500

    Better styling on info and settings tab

--- src/components/InfoMenu.tsx
+++ src/components/InfoMenu.tsx
@@ -2,8 +2,12 @@ export function InfoMenu() {
     //TODO: make name
     return (
         <div style={{
+            display: "flex",
             backgroundColor: "white",
+            padding: "10px",
             color: "black",
+            borderRadius: "20px",
+            border: "5px solid black",
         }}>
             <p>
                 X's Pathfinder
--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -2,11 +2,14 @@ export function SettingsMenu() {
     //TODO: make name
     return (
         <div style={{
+            display: "flex",
             backgroundColor: "white",
+            padding: "10px",
             color: "black",
+            borderRadius: "20px",
+            border: "5px solid black",
         }}>
             <p>
-                X's Pathfinder
                 Settings
             </p>
         </div>
