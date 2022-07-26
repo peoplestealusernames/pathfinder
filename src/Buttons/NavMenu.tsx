a7d673474a7ec1e7727d6a7acb5a5de1591635ed
Brett <bretthoganpr@gmail.com>
2022-07-25 19:06:44 -0500

    Made NavBar

--- src/Buttons/NavMenu.tsx
+++ src/Buttons/NavMenu.tsx
@@ -5,63 +5,15 @@ import { StyledTab } from "../components/StyledTab";
 import { NavInterface } from "../pathfinders/NavInterface";
 import { IoMdSkipForward, IoMdPlay, IoMdPause } from "react-icons/io"
 import { CgTimelapse } from "react-icons/cg";
+import { NavBar } from "../components/NavBar";
 
 export function NavMenu(props: {
     grid: LayerManger,
-    nav: NavInterface<any>,
-    selectorState: Selectable
+    nav: NavInterface<any>
 }) {
-    let Timer: NodeJS.Timer | undefined
-
-    function RunPath() {
-        if (!Timer) {
-            Timer = setInterval(() => {
-                if (props.nav.StepPath()) {
-                    clearInterval(Timer)
-                    Timer = undefined
-                }
-            }, 100)//TODO: pathfinder speed
-        }
-    }
-
-    function StopPath() {
-        clearInterval(Timer)
-        Timer = undefined
-    }
-
-
     return (
-        <StyledTab title="NavMenu">
-            <ContextButton
-                context={"Step Path"}
-                contextStyle={{ bottom: "-100%" }}
-                onMouseDown={() => { props.nav.StepPath() }}
-            >
-                <IoMdSkipForward />
-            </ContextButton>
-            <ContextButton
-                context={"Play pathfinder"}
-                contextStyle={{ bottom: "-100%" }}
-                buttonStyle={{ color: "green" }}
-                onMouseDown={() => { RunPath() }}
-            >
-                <IoMdPlay />
-            </ContextButton>
-            <ContextButton
-                context={"Stop pathfinder"}
-                contextStyle={{ bottom: "-100%" }}
-                buttonStyle={{ color: "red" }}
-                onMouseDown={() => { StopPath() }}
-            >
-                <IoMdPause />
-            </ContextButton>
-            <ContextButton
-                context={"Generate Path"}
-                contextStyle={{ bottom: "-100%" }}
-                onMouseDown={() => { props.nav.GeneratePath() }}
-            >
-                <CgTimelapse />
-            </ContextButton>
+        <StyledTab title="NavMenu" style={{ flexDirection: "column" }}>
+            <NavBar {...props} />
         </StyledTab>
     )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,70 @@
+import { useEffect } from "react"
+import { CgTimelapse } from "react-icons/cg"
+import { IoMdSkipForward, IoMdPlay, IoMdPause } from "react-icons/io"
+import { LayerManger } from "../2d/LayerManger"
+import { NavInterface } from "../pathfinders/NavInterface"
+import { ContextButton } from "./ContextButton"
+
+export function NavBar(props: {
+    grid: LayerManger,
+    nav: NavInterface<any>,
+}) {
+
+    let Timer: NodeJS.Timer | undefined
+
+    function RunPath() {
+        if (!Timer) {
+            Timer = setInterval(() => {
+                if (props.nav.StepPath()) {
+                    clearInterval(Timer)
+                    Timer = undefined
+                }
+            }, 100)//TODO: pathfinder speed
+        }
+    }
+
+    function StopPath() {
+        clearInterval(Timer)
+        Timer = undefined
+    }
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <ContextButton
+                context={"Step Path"}
+                contextStyle={{ bottom: "-100%" }}
+                onMouseDown={() => { props.nav.StepPath() }}
+            >
+                <IoMdSkipForward />
+            </ContextButton>
+            <ContextButton
+                context={"Play pathfinder"}
+                contextStyle={{ bottom: "-100%" }}
+                buttonStyle={{ color: "green" }}
+                onMouseDown={() => { RunPath() }}
+            >
+                <IoMdPlay />
+            </ContextButton>
+            <ContextButton
+                context={"Stop pathfinder"}
+                contextStyle={{ bottom: "-100%" }}
+                buttonStyle={{ color: "red" }}
+                onMouseDown={() => { StopPath() }}
+            >
+                <IoMdPause />
+            </ContextButton>
+            <ContextButton
+                context={"Generate Path"}
+                contextStyle={{ bottom: "-100%" }}
+                onMouseDown={() => { props.nav.GeneratePath() }}
+            >
+                <CgTimelapse />
+            </ContextButton>
+        </div>
+    )
+}
