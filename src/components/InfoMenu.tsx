9d2291115bc8e75b8b8f5d629eb18c7702e6e5d1
Brett <bretthoganpr@gmail.com>
2022-07-26 04:46:28 -0500

    Github Link in infoTab

--- src/components/InfoMenu.tsx
+++ src/components/InfoMenu.tsx
@@ -3,10 +3,17 @@ import { StyledTab } from "./StyledTab";
 export function InfoMenu() {
     //TODO: make name
     return (
-        <StyledTab title="Pathfinder">
+        <StyledTab
+            title="Pathfinder info"
+            style={{
+                flexDirection: "column"
+            }}>
             <span style={{ color: "white" }}>
                 MENU UNDER CONSTRUCTION
             </span>
+            <a href="https://github.com/peoplestealusernames/pathfinder">
+                Back to github!
+            </a>
         </StyledTab>
     )
 }
