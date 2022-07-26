894fef33f195df07dc41feb028b451c398d6596f
Brett <bretthoganpr@gmail.com>
2022-07-26 01:12:23 -0500

    InfoMenu added StyledTab

--- src/components/InfoMenu.tsx
+++ src/components/InfoMenu.tsx
@@ -1,18 +1,12 @@
+import { StyledTab } from "./StyledTab";
+
 export function InfoMenu() {
     //TODO: make name
     return (
-        <div style={{
-            display: "flex",
-            backgroundColor: "white",
-            padding: "10px",
-            color: "black",
-            borderRadius: "20px",
-            border: "5px solid black",
-        }}>
-            <p>
-                X's Pathfinder
-                Info
-            </p>
-        </div>
+        <StyledTab title="Pathfinder">
+            <span style={{ color: "white" }}>
+
+            </span>
+        </StyledTab>
     )
 }
