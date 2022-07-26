90e851946120f5b54db70b7f7a2e78ba4a0095ec
Brett <bretthoganpr@gmail.com>
2022-07-26 03:51:17 -0500

    TileCursor: Added indicator of where cursor is

--- src/components/Tiles/TileCursor.tsx
+++ src/components/Tiles/TileCursor.tsx
@@ -10,15 +10,21 @@ export function TileCursor(props: {
     return (
         <FollowMouse>
             <span style={{
-                width: "5px",
-                height: "5px",
+                position: "absolute",
+                left: "-2",
+                top: "-2",
+                width: "4px",
+                height: "4px",
                 borderRadius: "5px",
-                backgroundColor: "wheat"
+                backgroundColor: "white",
             }} />
             <TileRender style={{
+                position: "absolute",
+                left: "3px",
+                top: "3px",
                 width: "15px",
                 height: "15px",
-                border: "3px solid white",
+                border: "2px solid white",
                 borderRadius: "5px"
             }} tile={props.tile} />
         </FollowMouse>
