465a918ffda146b1704f0437969af1b72fea2ba3
Brett <bretthoganpr@gmail.com>
2022-07-26 01:12:29 -0500

    SettingsMenu added StyledTab

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -1,29 +1,18 @@
 import { LayerManger } from "../2d/LayerManger";
 import { ChangeDim } from "../Buttons/ChangeDim";
+import { StyledTab } from "./StyledTab";
 
 export function SettingsMenu(props: {
     grid: LayerManger
 }) {
 
     return (
-        <div style={{
-            display: "flex",
-            backgroundColor: "white",
-            padding: "10px",
-            color: "black",
-            borderRadius: "20px",
-            border: "5px solid black",
-            flexDirection: "column"
-        }}>
-            <p>
-                Settings
-            </p>
+        <StyledTab title="Settings">
             <ChangeDim key="ChangeX" inputType="number" initValue={props.grid.getWidth()} setter={(width: number) => { props.grid.setWidth(width) }} />
             <ChangeDim key="ChangeY" inputType="number" initValue={props.grid.getHeight()} setter={(height: number) => { props.grid.setHeight(height) }} />
             <button style={{ alignSelf: "center", display: "flex" }}
                 onMouseDown={() => { props.grid.clear(); }}
             >Reset</button>
-
-        </div>
+        </StyledTab>
     )
 }
