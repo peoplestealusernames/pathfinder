b5d9378b4a4145762be1612d1b8d3391f02bd217
Brett <bretthoganpr@gmail.com>
2022-07-26 00:59:29 -0500

    fixed: Generate maze button size of randomwall tab

--- src/Buttons/MazeMenu.tsx
+++ src/Buttons/MazeMenu.tsx
@@ -5,21 +5,22 @@ import { StyledTab } from "../components/StyledTab";
 import { NavInterface } from "../pathfinders/NavInterface";
 import { RandomWall } from "./RandomWall";
 
-
 export function MazeMenu(props: {
     grid: LayerManger,
     nav: NavInterface<any>,
 }) {
     return (
-        <StyledTab title="Obstacles Menu" style={{ margin: "2px" }}>
-            <ContextButton style={{
-                border: "2px solid white",
-                borderRadius: "2px"
-            }}
-                onMouseDown={() => { mazeGen(props.grid) }}
-            >
-                Generate Maze!
-            </ContextButton>
+        <StyledTab title="Obstacles Menu" style={{ margin: "2px", flexDirection: "row" }}>
+            <div>
+                <ContextButton style={{
+                    border: "2px solid white",
+                    borderRadius: "2px"
+                }}
+                    onMouseDown={() => { mazeGen(props.grid) }}
+                >
+                    Generate Maze!
+                </ContextButton>
+            </div>
             <RandomWall grid={props.grid} />
         </StyledTab >
     )
